2427e7592f2efe3346f076efc99f01f76779a81b
helpgoodforyouofficial-eng <helpgoodforyouofficial@gmail.com>
2026-09-03 17:59:15 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ const ASSETS = [
   './',
   'index.html',
   'manifest.json',
-  './style.css',
-  './script.js',        // 🆕 YE ADD KAREIN — offline ka asli hero
-  './icon-192.png',     // 🆕 (agar file hai)
-  './icon-512.png',     // 🆕 (agar file hai)
+  'style.css',
+  'script.js',        // 🆕 YE ADD KAREIN — offline ka asli hero
+  'icon-192.png',     // 🆕 (agar file hai)
+  'icon-512.png',     // 🆕 (agar file hai)
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
